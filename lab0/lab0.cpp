196bdad77e00d177919ebc6bad2e0a32c1680605
armenmerzaian <71707172+armenmerzaian@users.noreply.github.com>
2022-09-12 14:43:00 +0000

    final commit

--- lab0/lab0.cpp
+++ lab0/lab0.cpp
@@ -1,5 +1,5 @@
 
 unsigned long long factorial(unsigned int n){
-    if ( n == 0 || n == 1) return 2; //return 2 is error for github actions tester
+    if ( n == 0 || n == 1) return 1; 
     return n * factorial(n-1);
 }
