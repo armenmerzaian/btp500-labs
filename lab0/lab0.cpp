1d59873b692901a668884dd669c0ba3fd8b02a55
armenmerzaian <71707172+armenmerzaian@users.noreply.github.com>
2022-09-12 14:40:35 +0000

    added error

--- lab0/lab0.cpp
+++ lab0/lab0.cpp
@@ -1,5 +1,5 @@
 
 unsigned long long factorial(unsigned int n){
-    if ( n == 0 || n == 1) return 1;
+    if ( n == 0 || n == 1) return 2; //return 2 is error for github actions tester
     return n * factorial(n-1);
 }
