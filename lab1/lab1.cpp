41ebeafcb93a6bfbc0f66375690cff5cb8270316
armenmerzaian <71707172+armenmerzaian@users.noreply.github.com>
2022-09-18 21:21:41 +0000

    working interative factorial function

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -10,13 +10,18 @@
 /* remember to code these in an iterative only manner*/
 
 unsigned long long factorial (unsigned int n){
-
+    if (n == 0) return 1;
+    unsigned long long factorial = n;
+    for (int i = n - 1; i >= 1; i--){ 
+        factorial *= i;
+    }
+    return factorial;
 }
 
 unsigned long long power (unsigned int base, unsigned int n){
-
-
+    return 0;
 }
-unsigned long long fibonacci (unsigned int n){
 
+unsigned long long fibonacci (unsigned int n){
+    return 0;
 }
