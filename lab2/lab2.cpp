9b3605c485bb84eaf14e8453d4e965604949f38a
armenmerzaian <71707172+armenmerzaian@users.noreply.github.com>
2022-09-28 00:47:32 +0000

    all functions written and tested

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -2,21 +2,26 @@
 /*                                                */
 /*  Lab 2 Starter file                            */
 /*                                                */
-/*  Name:                                         */
-/*  Student number:                               */
+/*  Name:   Armen Merzaian                        */
+/*  Student number:    060207024                  */
 /*                                                */
 /**************************************************/
 
 /* remember to code these in a RECURSIVE only manner*/
 
 unsigned long long factorial (unsigned int n){
-
+    if(n <= 0) return 1;
+    unsigned long long val = n * factorial(n - 1);
+    return val;
 }
 
 unsigned long long power (unsigned int base, unsigned int n){
-
-
+    if(n <= 0) return 1;
+    unsigned long long val = base * power(base, n - 1);   
+    return val;
 }
 unsigned long long fibonacci (unsigned int n){
-
+    if(n <= 1) return n;
+    unsigned long long val = fibonacci(n - 1) + fibonacci(n - 2);
+    return val; 
 }
