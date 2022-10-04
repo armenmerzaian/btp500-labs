aaf8894f748c43641a013c0e9ed503a04ffeb889
armenmerzaian <71707172+armenmerzaian@users.noreply.github.com>
2022-10-04 12:02:04 +0000

    part 1 done

--- lab3/lab3.h
+++ lab3/lab3.h
@@ -1,26 +1,31 @@
 #include <iostream>
 
 template <typename T>
-class DList{
-	struct Node{
+class DList
+{
+	struct Node
+	{
 		T data_;
-		Node* next_;
-		Node* prev_;
-		Node(const T& data=T{},Node* next=nullptr, Node* prev=nullptr){
-			data_=data;
-			next_=next;
-			prev_=prev;
+		Node *next_;
+		Node *prev_;
+		Node(const T &data = T{}, Node *next = nullptr, Node *prev = nullptr)
+		{
+			data_ = data;
+			next_ = next;
+			prev_ = prev;
 		}
 	};
-	Node* front_;
-	Node* back_;
+	Node *front_;
+	Node *back_;
+
 public:
-	DList(){
-		front_=nullptr;
-		back_=nullptr;
+	DList()
+	{
+		front_ = nullptr;
+		back_ = nullptr;
 	}
-	void push_front(const T& data);
-	void push_back(const T& data);
+	void push_front(const T &data);
+	void push_back(const T &data);
 	void pop_front();
 	void pop_back();
 	int getData(int data[]) const;
@@ -28,49 +33,104 @@ public:
 	~DList();
 };
 
-
 template <typename T>
-void DList<T>::push_front(const T& data){
-
+void DList<T>::push_front(const T &data)
+{
+	Node *new_node = new Node(data, front_, nullptr);
+	if (front_ != nullptr)
+	{
+		front_->prev_ = new_node;
+	}
+	else
+	{
+		back_ = new_node;
+	}
+	front_ = new_node;
 }
 
 template <typename T>
-void DList<T>::push_back(const T& data){
-
+void DList<T>::push_back(const T &data)
+{
+	Node *new_node = new Node(data, nullptr, nullptr);
+	if (!(front_))
+	{
+		front_ = new_node;
+	}
+	else if (back_)
+	{
+		new_node->prev_ = back_;
+		back_->next_ = new_node;
+	}
+	back_ = new_node;
 }
 
 template <typename T>
-void DList<T>::pop_front(){
-
+void DList<T>::pop_front()
+{
+	Node *remove_node = front_;
+	front_ = remove_node->next_;
+	if (!front_)
+	{
+		back_ = nullptr;
+	}
+	else
+	{
+		front_->prev_ = nullptr;
+	}
+	delete remove_node;
 }
 
 template <typename T>
-void DList<T>::pop_back(){
-
+void DList<T>::pop_back()
+{
+	Node *remove_node = back_;
+	back_ = remove_node->prev_;
+	if (!back_)
+	{
+		front_ = nullptr;
+	}
+	else
+	{
+		back_->next_ = nullptr;
+	}
+	delete remove_node;
 }
 template <typename T>
-int DList<T>::getData(int data[]) const{
-	Node* curr=front_;
-	int numData=0;
-	while(curr!=nullptr){
-		data[numData++]=curr->data_;
-		curr=curr->next_;
+int DList<T>::getData(int data[]) const
+{
+	Node *curr = front_;
+	int numData = 0;
+	while (curr != nullptr)
+	{
+		data[numData++] = curr->data_;
+		curr = curr->next_;
 	}
 	return numData;
 }
 template <typename T>
-int DList<T>::getReverseData(int data[]) const{
-	Node* curr=back_;
-	int numData=0;
-	while(curr!=nullptr){
-		data[numData++]=curr->data_;
-		curr=curr->prev_;
+int DList<T>::getReverseData(int data[]) const
+{
+	Node *curr = back_;
+	int numData = 0;
+	while (curr != nullptr)
+	{
+		data[numData++] = curr->data_;
+		curr = curr->prev_;
 	}
 	return numData;
 }
 template <typename T>
-DList<T>::~DList(){
+DList<T>::~DList()
+{
+	Node *current = front_;
+	Node *next;
 
+	while (current != nullptr)
+	{
+		next = current->next_;
+		delete current;
+		current = next;
+	}
 }
 template <typename T>
 class Sentinel{
