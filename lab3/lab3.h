012dd4cff059dc05487e7f933599cc6a418bf4ec
armenmerzaian <71707172+armenmerzaian@users.noreply.github.com>
2022-10-04 12:15:22 +0000

    updates made

--- lab3/lab3.h
+++ lab3/lab3.h
@@ -67,33 +67,31 @@ void DList<T>::push_back(const T &data)
 template <typename T>
 void DList<T>::pop_front()
 {
-	Node *remove_node = front_;
-	front_ = remove_node->next_;
-	if (!front_)
-	{
-		back_ = nullptr;
+	if (front_){
+		Node *remove_node = front_;
+		front_ = front_->next_;
+		if (front_ == nullptr){ 
+			back_ = nullptr;
+		}else {
+			front_->prev_ = nullptr;
+		}
+		delete remove_node;
 	}
-	else
-	{
-		front_->prev_ = nullptr;
-	}
-	delete remove_node;
 }
 
 template <typename T>
 void DList<T>::pop_back()
 {
-	Node *remove_node = back_;
-	back_ = remove_node->prev_;
-	if (!back_)
-	{
-		front_ = nullptr;
+	if (back_){
+		Node *remove_node = back_;
+		back_ = back_->prev_;
+		if (back_ == nullptr){
+			front_ = nullptr;
+		}else {
+			back_->next_ = nullptr;
+		}
+		delete remove_node;
 	}
-	else
-	{
-		back_->next_ = nullptr;
-	}
-	delete remove_node;
 }
 template <typename T>
 int DList<T>::getData(int data[]) const
@@ -165,22 +163,38 @@ public:
 
 template <typename T>
 void Sentinel<T>::push_front(const T& data){
-
+	Node *new_node = new Node(data, front_->next_, front_);
+	front_->next_->prev_ = new_node;
+	front_->next_ = new_node;
 }
 
 template <typename T>
 void Sentinel<T>::push_back(const T& data){
-
+	Node *new_node = new Node(data, back_, back_->prev_);
+	back_->prev_->next_ = new_node;
+	back_->prev_ = new_node;
 }
 
 template <typename T>
 void Sentinel<T>::pop_front(){
-
+	if (front_->next_ != back_)
+	{
+		Node *remove_node = front_->next_;
+		front_->next_ = remove_node->next_;
+		front_->next_->prev_ = front_;
+		delete remove_node;
+	}
 }
 
 template <typename T>
 void Sentinel<T>::pop_back(){
-
+	if (back_->prev_ != front_)
+	{
+		Node *remove_node = back_->prev_;
+		back_->prev_ = remove_node->prev_;
+		back_->prev_->next_ = back_;
+		delete remove_node;
+	}
 }
 
 template <typename T>
@@ -207,5 +221,13 @@ int Sentinel<T>::getReverseData(int data[]) const{
 
 template <typename T>
 Sentinel<T>::~Sentinel(){
+	Node *current = front_;
+	Node *next;
 
+	while (current != nullptr)
+	{
+		next = current->next_;
+		delete current;
+		current = next;
+	}
 }
