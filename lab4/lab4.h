f02356b52c6aa1f5e831429c21b599046342bef0
armenmerzaian <71707172+armenmerzaian@users.noreply.github.com>
2022-10-10 10:45:08 +0000

    first commit with errors

--- lab4/lab4.h
+++ lab4/lab4.h
@@ -23,52 +23,168 @@ public:
 	~DList();
 
 	class const_iterator{
+		friend class DList;
+	protected:
+		const DList *myList_;
 		Node* curr_;
-		const_iterator(Node* n){
+		const_iterator(Node* n, const DList *theList){
 			curr_=n;
+			myList_ = theList;
 		}
 	public:
 		const_iterator(){
 			curr_=nullptr;
+			myList_ = nullptr;
 		}
-		const_iterator& operator++(){}
-		const_iterator operator++(int){}
-		const_iterator& operator--(){}
-		const_iterator operator--(int){}
-		bool operator==(const_iterator rhs){}
-		bool operator!=(const_iterator rhs){}
-		const T& operator*()const{}
+		const_iterator& operator++(){
+			curr_ = curr_->next_;
+			return *this;
+		}
+		const_iterator operator++(int){
+			const_iterator old = *this;
+			curr_ = curr->next;
+			return old;
+		}
+		const_iterator& operator--(){
+			if (curr_)
+			{
+				curr_ = curr_->prev_;
+			}
+			else
+			{
+				if (myList_)
+				{
+					curr_ = myList->back_;
+				}
+			}
+		}
+		const_iterator operator--(int){
+			Node *old = *this;
+			if (curr_)
+			{
+				curr_ = curr +->prev_;
+			}
+			else
+			{
+				if (myList_)
+				{
+					curr_ = myList->back_;
+				}
+			}
+			return old;
+		}
+		bool operator==(const_iterator rhs){
+			bool compares = false;
+			if (myList_ == rhs.myList_ && curr_ == rhs.curr_)
+			{
+				compares = true;
+			}
+			return compares;
+		}
+		bool operator!=(const_iterator rhs){
+			return !(*this == rhs);
+		}
+		const T& operator*()const{
+			return curr_->data_;
+		}
 	};
 	class iterator:public const_iterator{
+	private:
+		iterator(Node* curr, DList *theList){
+			this->curr_ = curr;
+			this->myList_ = theList;
+		}
 	public:
-		iterator();
-		iterator& operator++(){}
-		iterator operator++(int){}
-		iterator& operator--(){}
-		iterator operator--(int){}
-		T& operator*(){}
-		const T& operator*()const{}
+		iterator(){
+			this->myList_ = nullptr;
+			this->curr_ = nullptr;
+		}
+		iterator& operator++(){
+			this->curr_ = this->curr_->next_;
+		}
+		iterator operator++(int){
+			iterator old = *this;
+			this->curr_ = this->curr_->next;
+			return old;
+		}
+		iterator& operator--(){
+			if (this->curr_)
+			{
+				this->curr_ = this->curr_->prev_;
+			}
+			else
+			{
+				if (myList_)
+				{
+					this->curr_ = this->myList->back_;
+				}
+			}
+		}
+		iterator operator--(int){
+			Node *old = *this;
+			if (this->curr_)
+			{
+				this->curr_ = this->curr_->prev_;
+			}
+			else
+			{
+				if (this->myList_)
+				{
+					this->curr_ = this->myList->back_;
+				}
+			}
+			return old;
+		}
+		T& operator*(){
+			return this->curr_->data_;
+		}
+		const T& operator*()const{
+			return this->curr_->data_;
+		}
 
 	};
 
-	const_iterator cbegin() const{	}
-
-	iterator begin(){	}
-
-	const_iterator cend() const{}
-	iterator end(){}
+	const_iterator cbegin() const{
+		return const_iterator(front_, this);
+	}
+	iterator begin(){
+		return iterator(front_, this);
+	}
+	const_iterator cend() const{
+		return const_iterator(nullptr, this);
+	}
+	iterator end(){
+		return iterator(nullptr, this);
+	}
 };
 
 
 template <typename T>
-void DList<T>::push_front(const T& data){
-
+void DList<T>::push_front(const T& data)
+{
+	Node *new_node = new Node(data, front_, nullptr);
+	if (front_ != nullptr)
+	{
+		front_->prev_ = new_node;
+	}
+	else
+	{
+		back_ = new_node;
+	}
+	front_ = new_node;
 }
-
 
 template <typename T>
 DList<T>::~DList(){
+	Node *current = front_;
+	Node *next;
 
+	while (current != nullptr)
+	{
+		next = current->next_;
+		delete current;
+		current = next;
+	}
 }
 
 
@@ -97,40 +213,161 @@ public:
 	~Sentinel();
 
 	class const_iterator{
+		friend class Sentinel;
+	protected:
+		const Sentinel *myList_;
+		Node *curr_;
+		const_iterator(Node *n, const Sentinel *theList)
+		{
+			curr_ = n;
+			myList_ = theList;
+		}
 	public:
-		const_iterator(){}
-		const_iterator& operator++(){}
-		const_iterator operator++(int){}
-		const_iterator& operator--(){}
-		const_iterator operator--(int){}
-		bool operator==(const_iterator rhs){}
-		bool operator!=(const_iterator rhs){}
-		const T& operator*()const{}
+		const_iterator(){
+			curr_ = nullptr;
+			myList_ = nullptr;
+		}
+		const_iterator& operator++(){
+			curr_ = curr_->next_;
+			return *this;
+		}
+		const_iterator operator++(int){
+			const_iterator old = *this;
+			curr_ = curr->next;
+			return old;
+		}
+		const_iterator& operator--(){
+			if (curr_)
+			{
+				curr_ = curr_->prev_;
+			}
+			else
+			{
+				if (myList_)
+				{
+					curr_ = myList->back_;
+				}
+			}
+		}
+		const_iterator operator--(int){
+			Node *old = *this;
+			if (curr_)
+			{
+				curr_ = curr +->prev_;
+			}
+			else
+			{
+				if (myList_)
+				{
+					curr_ = myList->back_;
+				}
+			}
+			return old;
+		}
+		bool operator==(const_iterator rhs){
+			bool compares = false;
+			if (myList_ == rhs.myList_ && curr_ == rhs.curr_)
+			{
+				compares = true;
+			}
+			return compares;
+		}
+		bool operator!=(const_iterator rhs){
+			return !(*this == rhs);
+		}
+		const T& operator*()const{
+			return curr_->data_;
+		}
 	};
 	class iterator:public const_iterator{
+	private:
+		iterator(Node *curr, Sentinel *theList)
+		{
+			this->curr_ = curr;
+			this->myList_ = theList;
+		}
+
 	public:
-		iterator();
-		iterator& operator++(){}
-		iterator operator++(int){}
-		iterator& operator--(){}
-		iterator operator--(int){}
-		T& operator*(){}
-		const T& operator*()const{}
+		iterator(){
+			this->myList_ = nullptr;
+			this->curr_ = nullptr;
+		}
+		iterator& operator++(){
+			this->curr_ = this->curr_->next_;
+		}
+		iterator operator++(int){
+			iterator old = *this;
+			this->curr_ = this->curr_->next;
+			return old;
+		}
+		iterator& operator--(){
+			if (this->curr_)
+			{
+				this->curr_ = this->curr_->prev_;
+			}
+			else
+			{
+				if (myList_)
+				{
+					this->curr_ = this->myList->back_;
+				}
+			}
+		}
+		iterator operator--(int){
+			Node *old = *this;
+			if (this->curr_)
+			{
+				this->curr_ = this->curr_->prev_;
+			}
+			else
+			{
+				if (this->myList_)
+				{
+					this->curr_ = this->myList->back_;
+				}
+			}
+			return old;
+		}
+		T& operator*(){
+			return this->curr_->data_;
+		}
+		const T& operator*()const{
+			return this->curr_->data_;
+		}
 	};
-	const_iterator cbegin() const{}
-	iterator begin(){}
-	const_iterator cend() const{}
-	iterator end(){}
+	const_iterator cbegin() const{
+		return const_iterator(front_, this);
+	}
+	iterator begin(){
+		return iterator(front_, this);
+	}
+	const_iterator cend() const{
+		return const_iterator(nullptr, this);
+	}
+
+	iterator end(){
+		return iterator(nullptr, this);
+	}
 };
 
 
 template <typename T>
 void Sentinel<T>::push_front(const T& data){
-
+	Node *new_node = new Node(data, front_->next_, front_);
+	front_->next_->prev_ = new_node;
+	front_->next_ = new_node;
 }
 
 
 template <typename T>
 Sentinel<T>::~Sentinel(){
+	Node *current = front_;
+	Node *next;
 
+	while (current != nullptr)
+	{
+		next = current->next_;
+		delete current;
+		current = next;
+	}
 }
